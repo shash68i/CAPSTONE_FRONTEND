88dd1daa9e22e1085225bb9f8b9911f9a3f4d0db
shash68i <theshashank6870@gmail.com>
2022-04-10 10:48:20 +0530

    Initial redux-toolkit configuration setup

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import authSlice from "../slices/authSlice";
+
+
+const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer,
+  },
+});
+
+
+
+export default store;
