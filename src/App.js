70ea09c000f7c5830689b45f2957901db2188bab
shash68i <theshashank6870@gmail.com>
2022-04-10 10:54:10 +0530

    Added Routes and authentication

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -7,20 +8,42 @@ import "./App.css";
 import "./style/utils.css";
 import Login from "./pages/Login";
 
-// import Navbar from "./components/Navigation/Navbar";
+import Navbar from "./components/Navigation/Navbar";
+import DetailPostCard from "./pages/DetailPostCard";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+import CreatePost from "./pages/CreatePost";
+import PrivateRoute from "./components/Route/PrivateRoute";
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <Fragment>
-      {/* <Navbar /> */}
 
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/register" component={Signup} />
-          <Route exact path="/login" component={Login} />
+      <Routes>
+        {/* Unauthorized Routes */}
+        <Route path="register" element={<Signup />} />
+        <Route path="login" element={<Login />} />
 
-          
-        </Switch>
+        {/* Protected Routes */}
+        <Route path="/" element={<PrivateRoute component={Home} />} />
+        <Route
+          path="post"
+          element={<PrivateRoute component={DetailPostCard} />}
+        />
+
+        <Route path="profile" element={<PrivateRoute component={Profile} />} />
+
+        <Route
+          path="edit-profile"
+          element={<PrivateRoute component={EditProfile} />}
+        />
+
+        <Route
+          path="create-post"
+          element={<PrivateRoute component={CreatePost} />}
+        />
+      </Routes>
     </Fragment>
   );
 }
