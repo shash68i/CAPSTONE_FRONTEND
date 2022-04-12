b995082800cbe5f19e6e5ef7df0b0b4e1d6e0070
shash68i <theshashank6870@gmail.com>
2022-04-12 10:49:41 +0530

    Added auth api endpoint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import "./App.css";
-import "./style/utils.css";
 import Login from "./pages/Login";
 
 import Navbar from "./components/Navigation/Navbar";
@@ -14,11 +13,33 @@ import Profile from "./pages/Profile";
 import EditProfile from "./pages/EditProfile";
 import CreatePost from "./pages/CreatePost";
 import PrivateRoute from "./components/Route/PrivateRoute";
+import { ToastContainer } from "react-toastify";
+import setAuthToken from "./utils/setAuthToken";
+import { authActions, loadUser } from "./slices/authSlice";
 
 function App() {
-  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token);
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener("storage", () => {
+      if (!localStorage.token) dispatch(authActions.logoutUser());
+    });
+  }, []);
+
   return (
     <Fragment>
+      <ToastContainer />
 
       <Routes>
         {/* Unauthorized Routes */}
