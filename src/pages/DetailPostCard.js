3a8f7917f00872af47ca1bd7890baf976f3700ec
shash68i <theshashank6870@gmail.com>
2022-04-21 11:16:08 +0530

    API integrated in Detail Post Card

--- src/pages/DetailPostCard.js
+++ src/pages/DetailPostCard.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useEffect } from "react";
 
 import {
   AccountCircleTwoTone,
@@ -13,124 +13,172 @@ import Carousel from "react-material-ui-carousel";
 
 import "../components/PostCard/PostCard.css";
 import Comments from "../components/Comments/Comments";
+import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { getPost } from "../slices/postSlice";
+import { NavLink } from "react-router-dom";
+import Suggestion from "../components/Suggestion/Suggestion";
+import { locationsGroup } from "../location";
+
+import "./DetailPostCard.css";
+import NearbyLocation from "../components/NearbyLocation/NearbyLocation";
 
 function DetailPostCard() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const post = useSelector((state) => state.post.post);
+
+  // const timestamp = new Date(post.date);
+  const nearbyLocations = locationsGroup
+    .find((locationGroup) => locationGroup.includes("Ranchi, Jharkhand"))
+    .filter((location) => location !== "Ranchi, Jharkhand");
+
+  console.log(nearbyLocations, "post id");
+
+  useEffect(() => {
+    dispatch(getPost(id));
+  }, [id]);
+
   return (
-    <Fragment>
-      <div className="detail-post-card">
-        <div className="post-card__title">
-          <div className="profile-pic">
-            <img src="https://picsum.photos/200/300" alt="Profile Pic" />
-            {/* <AccountCircleTwoTone sx={{ fontSize: "4.5rem", color: "#4d4d4d" }} /> */}
+    post && (
+      <Fragment>
+        <div className="detail-post-card">
+          <div className="post-card__title">
+            <div className="profile-pic">
+              <img src="https://picsum.photos/200/300" alt="Profile Pic" />
+              {/* <AccountCircleTwoTone sx={{ fontSize: "4.5rem", color: "#4d4d4d" }} /> */}
+            </div>
+
+            <div className="user-info__group">
+              <div className="user-location">
+                <FmdGoodOutlined
+                  sx={{
+                    fontSize: "1.7rem",
+                    marginRight: "0.2rem",
+                    color: "#ea4335",
+                  }}
+                />{" "}
+                {post.location}
+              </div>
+              <div className="user-details">
+                <NavLink
+                  to={`/users/${post.user}`}
+                  state={{
+                    first_name: post.first_name,
+                    last_name: post.last_name,
+                    username: post.username,
+                  }}
+                >
+                  <span className="name">
+                    <strong>
+                      {post.first_name} {post.last_name}
+                    </strong>{" "}
+                  </span>
+                </NavLink>
+                <NavLink
+                  to={`/users/${post.user}`}
+                  state={{
+                    first_name: post.first_name,
+                    last_name: post.last_name,
+                    username: post.username,
+                  }}
+                >
+                  <span className="username">@{post.username} </span>
+                </NavLink>
+                <span className="timestamp">
+                  &#8226;{Date(post.date).slice(4, 15)}
+                </span>
+              </div>
+            </div>
           </div>
 
-          <div className="user-info__group">
-            <div className="user-location">
-              <FmdGoodOutlined
-                sx={{
-                  fontSize: "1.7rem",
-                  marginRight: "0.2rem",
-                  color: "#ea4335",
-                }}
-              />{" "}
-              Ranchi Jharkhand
-            </div>
-            <div className="user-details">
-              <span className="name">
-                <strong>Shashank Kumar</strong>{" "}
+          <div>
+            <Carousel
+              style={{ height: "40rem" }}
+              className="detail-post-card__images"
+              autoPlay={false}
+              animation={"slide"}
+              navButtonsAlwaysVisible={true}
+              indicatorContainerProps={{
+                style: {
+                  position: "absolute",
+                  bottom: "10px",
+                  zIndex: 2, // 5
+                },
+              }}
+              navButtonsProps={{
+                style: {
+                  backgroundColor: "#c0bdc2",
+                  backdropFilter: "blur(5px)",
+                  color: "black",
+                },
+              }}
+            >
+              {post.images.map((image, index) => (
+                <img src={image} key={index} alt="Post Images" />
+              ))}
+            </Carousel>
+          </div>
+
+          <div className="post-card__text">
+            <strong>Shashank Kumar</strong> Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. In metus vulputate eu scelerisque.{" "}
+            <br />
+            <br />
+            Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus.
+            Neque ornare aenean euismod elementum nisi. Sollicitudin tempor id
+            eu nisl nunc mi. Nibh mauris cursus mattis molestie a. Pellentesque
+            pulvinar pellentesque habitant morbi. Adipiscing elit duis tristique
+            sollicitudin nibh. Nibh nisl condimentum id venenatis a condimentum
+            vitae sapien. Interdum varius sit amet mattis vulputate enim. In
+            ante metus dictum at tempor.
+            <div className="post-card__actions-info">
+              <span>
+                <FavoriteOutlined
+                  sx={{
+                    fontSize: "1.85rem",
+                    marginRight: "0.1rem",
+                    color: "#ed4956",
+                  }}
+                />
+                Arjit and 63 others
               </span>
-              <span className="username">@shash68i </span>
-              <span className="timestamp">&#8226;28 Jan</span>
+
+              <span>
+                <ModeCommentOutlined
+                  sx={{ fontSize: "1.85rem", marginRight: "0.1rem" }}
+                />
+                21 comments
+              </span>
             </div>
           </div>
-        </div>
 
-        <div>
-          <Carousel
-            style={{ height: "40rem" }}
-            className="detail-post-card__images"
-            autoPlay={false}
-            animation={"slide"}
-            navButtonsAlwaysVisible={true}
-            indicatorContainerProps={{
-              style: {
-                position: "absolute",
-                bottom: "10px",
-                zIndex: 2, // 5
-              },
-            }}
-            navButtonsProps={{
-              style: {
-                backgroundColor: "#c0bdc2",
-                backdropFilter: "blur(5px)",
-                color: "black",
-              },
-            }}
-          >
-            <img src="https://picsum.photos/2080" alt="Post Images" />
-            <img src="https://picsum.photos/2010" alt="Post Images" />
-            <img src="https://picsum.photos/2000" alt="Post Images" />
-            <img src="https://picsum.photos/2020" alt="Post Images" />
-          </Carousel>
-        </div>
-
-        <div className="post-card__text">
-          <strong>Shashank Kumar</strong> Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. In metus vulputate eu scelerisque.{" "}
-          <br />
-          <br />
-          Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Neque
-          ornare aenean euismod elementum nisi. Sollicitudin tempor id eu nisl
-          nunc mi. Nibh mauris cursus mattis molestie a. Pellentesque pulvinar
-          pellentesque habitant morbi. Adipiscing elit duis tristique
-          sollicitudin nibh. Nibh nisl condimentum id venenatis a condimentum
-          vitae sapien. Interdum varius sit amet mattis vulputate enim. In ante
-          metus dictum at tempor.
-          <div className="post-card__actions-info">
-            <span>
-              <FavoriteOutlined
+          <div className="post-card__actions">
+            <span className="action-items">
+              <FavoriteBorderOutlined
+                sx={{ fontSize: "2.7rem", margin: "1rem" }}
+              />
+            </span>
+            <span className="action-items">
+              <ModeCommentOutlined
                 sx={{
-                  fontSize: "1.85rem",
-                  marginRight: "0.1rem",
-                  color: "#ed4956",
+                  fontSize: "2.4rem",
+                  margin: "1rem",
+                  fontWeight: "400",
                 }}
               />
-              Arjit and 63 others
-            </span>
-
-            <span>
-              <ModeCommentOutlined
-                sx={{ fontSize: "1.85rem", marginRight: "0.1rem" }}
-              />
-              21 comments
             </span>
           </div>
         </div>
 
-        <div className="post-card__actions">
-          <span className="action-items">
-            <FavoriteBorderOutlined
-              sx={{ fontSize: "2.7rem", margin: "1rem" }}
-            />
-          </span>
-          <span className="action-items">
-            <ModeCommentOutlined
-              sx={{ fontSize: "2.4rem", margin: "1rem", fontWeight: "400" }}
-            />
-          </span>
+        <Comments comments={post.comments} postId={id} />
+        <div className="nearby-location-section">
+          <NearbyLocation locationOptions={nearbyLocations} />
         </div>
-
-        {/* <hr />
-        <div className="post-card__comments">
-            <input type="text" name="post-comment" placeholder="Post your comment" />
-            <input type="submit" value="Post" />
-        </div>
-        <hr /> */}
-      </div>
-
-      <Comments />
-    </Fragment>
+      </Fragment>
+    )
   );
 }
 
