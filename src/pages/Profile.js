e8f2c6e2df4286a40204bbf4a0a76571242fddda
shash68i <theshashank6870@gmail.com>
2022-04-21 11:21:18 +0530

    Profile and User profile Component created and api integration added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect } from "react";
+import { NavLink, Navigate } from "react-router-dom";
 import {
   EmailOutlined,
   FacebookOutlined,
@@ -10,56 +10,70 @@ import {
 
 import "./Profile.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { getMyPosts, getMyProfile} from "../slices/userSlice";
+
 import PostCard from "../components/PostCard/PostCard";
 
-const Profile = () => {
-  return (
+export default function Profile() {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.auth.user);
+  const profile = useSelector((state) => state.user.myProfile);
+  const posts = useSelector((state) => state.user.myPosts);
+  const loading = useSelector((state) => state.user.loading);
+
+  
+
+  useEffect(() => {
+    if (user) {
+      dispatch(getMyProfile());
+      dispatch(getMyPosts(user));
+    }
+  }, []);
+
+  useEffect(() => {
+    if(profile=== null) {
+      return <Navigate to="/create-profile" />
+    }
+    
+  }, [profile])
+
+
+  
+
+  return profile && (
     <div className="profile-page">
       <div className="profile-card">
         <div className="profile-card-left">
           <span className="profile-large-pic">
-            <img src="https://picsum.photos/200/300" alt="Profile Pic" />
+            <img src={profile.profile_pic} alt="Profile Pic" />
           </span>
         </div>
 
         <div className="profile-card-right">
-          <span className="profile__name">Arjit Kaur Arora</span>
-          <span className="profile__username">@shash68i </span>
+          <span className="profile__name">{user.first_name} {user.last_name}</span>
+          <span className="profile__username">@{user.username} </span>
           <span className="profile__socials">
-            <EmailOutlined className="email" sx={{ fontSize: "2rem"}} />
-            <FmdGoodOutlined className="address" sx= {{ fontSize: "2rem"}} />
-            <Twitter className="twitter" sx={{ fontSize: "2rem"}} />
-            <FacebookOutlined className="facebook" sx={{ fontSize: "2rem"}} />
-            <Instagram className="instagram" sx={{ fontSize: "2rem"}} />
+            <EmailOutlined className="email" sx={{ fontSize: "2rem" }} />
+            <FmdGoodOutlined className="address" sx={{ fontSize: "2rem" }} />
+            <Twitter className="twitter" sx={{ fontSize: "2rem" }} />
+            <FacebookOutlined className="facebook" sx={{ fontSize: "2rem" }} />
+            <Instagram className="instagram" sx={{ fontSize: "2rem" }} />
           </span>
 
           <span className="profile__bio">
             <br />
-            UI/UX Developer lorem ipsum dator
-            <br />
-            Love To travel
-            <br />
-            Travel Guide
-            <br />
-            Book for Rs. 500
+            {profile.bio}
           </span>
 
-          <NavLink to="/edit-profile" className="profile-edit"><button className="edit-profile-btn">Edit Profile</button></NavLink>
+          <NavLink to="/edit-profile" className="profile-edit">
+            <button className="edit-profile-btn">Edit Profile</button>
+          </NavLink>
         </div>
-
-        {/* <div className="comment__text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. In metus
-          vulputate eu scelerisque. <br />
-          Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Neque
-          ornare aenean euismod elementum nisi. Sollicitudin tempor id eu nisl
-          nunc mi. Nibh mauris cursus mattis molestie a.
-        </div> */}
       </div>
 
-      <PostCard />
+      {posts.map((post) => <PostCard post={post} key={post._id}/>)}
     </div>
   );
-};
-
-export default Profile;
+}
---
+++ src/pages/UserProfile.js
@@ -0,0 +1,86 @@
+import React, { useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+import { useParams } from "react-router";
+
+import {
+  EmailOutlined,
+  FacebookOutlined,
+  FmdGoodOutlined,
+  Instagram,
+  Twitter,
+} from "@mui/icons-material";
+
+import "./Profile.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getUserPosts, getUserProfile } from "../slices/userSlice";
+
+import PostCard from "../components/PostCard/PostCard";
+
+const UserProfile = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { state } = useLocation();
+  const { first_name, last_name, username } = state;
+
+  const profile = useSelector((state) => state.user.userProfile);
+  const posts = useSelector((state) => state.user.userPosts);
+
+
+  useEffect(() => {
+    dispatch(getUserProfile(id));
+    dispatch(getUserPosts(id));
+  }, []);
+  return (
+    profile &&
+    posts && (
+      <div className="profile-page">
+        <div className="profile-card">
+          <div className="profile-card-left">
+            <span className="profile-large-pic">
+              <img src={profile.profile_pic} alt="Profile Pic" />
+            </span>
+          </div>
+
+          <div className="profile-card-right">
+            <span className="profile__name">
+              {first_name} {last_name}
+            </span>
+            <span className="profile__username">@{username} </span>
+            <span className="profile__socials">
+              <EmailOutlined className="email" sx={{ fontSize: "2rem" }} />
+              <FmdGoodOutlined className="address" sx={{ fontSize: "2rem" }} />
+              <Twitter className="twitter" sx={{ fontSize: "2rem" }} />
+              <FacebookOutlined
+                className="facebook"
+                sx={{ fontSize: "2rem" }}
+              />
+              <Instagram className="instagram" sx={{ fontSize: "2rem" }} />
+            </span>
+
+            <span className="profile__bio">
+              <br />
+              {profile.bio}
+            </span>
+          </div>
+
+          {/* <div className="comment__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. In metus
+          vulputate eu scelerisque. <br />
+          Amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus. Neque
+          ornare aenean euismod elementum nisi. Sollicitudin tempor id eu nisl
+          nunc mi. Nibh mauris cursus mattis molestie a.
+        </div> */}
+        </div>
+
+        {posts.length > 0 && posts.map((post) => (
+          <PostCard post={post} key={post._id}/>
+        ))}
+      </div>
+    )
+  );
+};
+
+export default UserProfile;
